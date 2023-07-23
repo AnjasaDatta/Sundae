bb25ca705d56088715f260cde476e0b34876dd04
AnjasaDatta <anjasa.datta1996@gmail.com>
2023-07-23 10:00:21 +0530

    added toppings section

--- sundaes-on-demand/src/mocks/handlers.js
+++ sundaes-on-demand/src/mocks/handlers.js
@@ -7,5 +7,14 @@ export const handlers = [
                 {name:'Vanilla',imagePath : '/images/vanilla.png'},
             ])
         )
-    })
+    }),
+    rest.get("http://localhost:30030/toppings",(res,req,ctx)=>{
+        return res(
+            ctx.json([
+                {name:'Cherries',imagePath : '/images/cherries.png'},
+                {name:'M&Ms',imagePath : '/images/m-and-ms.png'},
+                {name:'Hot Fudge',imagePath : '/images/hot-fudge.png'},
+            ])
+        )
+    }),
 ]
--- sundaes-on-demand/src/pages/entry/ToppingOption.jsx
+++ sundaes-on-demand/src/pages/entry/ToppingOption.jsx
@@ -1,13 +1,13 @@
 import Col from "react-bootstrap/Col";
 
-const ScoopOptions = ({ name, image }) => (
- 
+export default function ToppingOptions({ name, image }) {
+  return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
         style={{ width: "75%" }}
         src={`http://localhost:3030/${image}`}
-        alt={`${name} scoop`}
+        alt={`${name} topping`}
       />
     </Col>
   );
-export default ScoopOptions;
+}
